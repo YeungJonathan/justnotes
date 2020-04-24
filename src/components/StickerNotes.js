7bcbc7d05662179f9a77cde8c4a641cd8f5ec621
Jonathan Yeung <jonathanyeung1997@gmail.com>
2020-04-25 00:23:18 +1000

    stickerNotes

--- src/components/StickerNotes.js
+++ src/components/StickerNotes.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import '../styles/StickyNotes.css';
 
 const StickerNotes = ({notesList}) => {
   return (
     <>
         {notesList.map(note => (
-            <div key={note.id} style={{height:'400px', width:'250px', backgroundColor: 'lightblue'}}>
-                <h3>
-                    {note.title}
-                </h3>
-                <p>
+            <div key={note.id} className="NotesContainer" >
+                <input type="text" className="title" value={note.title}/>
+                <p className="description">
                     {note.description}
                 </p>
             </div>
