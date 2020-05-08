bb37cb2455c4e8890e01dba532abf71ce535b036
Jonathan Yeung <jonathanyeung1997@gmail.com>
2020-05-08 13:21:33 +1000

    refactoring

--- src/reducers/StickerNotes.js
+++ src/reducers/StickerNotes.js
@@ -4,66 +4,53 @@ const defaultStore = {
     notesList: []
 }
 
+const addNotes = (state) => {
+	const offSet = state.id * 5;
+	const newNote = {
+		id: state.id,
+		title: 'Untitled',
+		description: 'Enter here ...',
+		left: 150 + offSet,
+		top: 150 + offSet,
+	}
+	return {
+		id: state.id + 1,
+		notesList: [...state.notesList, newNote]
+	}
+}
+
+const updateDetails = (state, action) => {
+	return Object.assign({}, state, {
+		notesList: state.notesList.map(note => {
+			if (note.id === action.payload.id){
+
+				switch(action.type){
+					case 'UPDATE_TITLE':
+						const { title } = action.payload;
+						return {...note, title}
+					case 'UPDATE_DESCRIPTION':
+						const { description } = action.payload;
+						return {...note, description}
+					case 'UPDATE_LOCATION':
+						const { x: left, y: top } = action.payload;
+						return {...note, left, top}
+					default:
+						return note
+				}	
+			}
+			return note
+		})
+	})
+}
+
 export default function StickerNotes(state = defaultStore, action) {
     switch (action.type) {
 		case 'ADD_NOTES':
-			const offSet = state.id * 5;
-			const newNote = {
-				id: state.id,
-				title: 'Untitled',
-				description: 'Enter here ...',
-				left: 150 + offSet,
-				top: 150 + offSet,
-			}
-			return {
-				id: state.id + 1,
-				notesList: [...state.notesList, newNote]
-			}
+			return addNotes(state)
 		case 'UPDATE_TITLE':
-			return Object.assign({}, state, {
-				notesList: state.notesList.map(note => {
-					if (note.id === action.payload.id){
-						return {
-							id: note.id,
-							title: action.payload.title,
-							description: note.description,
-							left: note.left,
-							top: note.top,
-						}
-					}
-					return note
-				})
-			})
 		case 'UPDATE_DESCRIPTION':
-			return Object.assign({}, state, {
-				notesList: state.notesList.map(note => {
-					if (note.id === action.payload.id){
-						return {
-							id: note.id,
-							title: note.title,
-							description: action.payload.description,
-							left: note.left,
-							top: note.top,
-						}
-					}
-					return note
-				})
-			})
 		case 'UPDATE_LOCATION':
-			return Object.assign({}, state, {
-				notesList: state.notesList.map(note => {
-					if (note.id === action.payload.id){
-						return {
-							id: note.id,
-							title: note.title,
-							description: note.description,
-							left: action.payload.x,
-							top: action.payload.y,
-						}
-					}
-					return note
-				})
-			})
+			return updateDetails(state, action)
 		case 'DELETE_NOTE':
 			const filteredNotes = state.notesList.filter(note => note.id !== action.payload.id);
 			return {
